78f2bbc09599879cb80d15ff46d37dcd46d8fc8a
miztikk <jackbianchi15@gmail.com>
2015-05-11 11:42:01 -0700

    Button hovering functionality, no title display text yet

--- src/Display.java
+++ src/Display.java
@@ -2,19 +2,28 @@ import processing.core.PApplet;
 
 public class Display {
 	private PApplet parent;
+	private Button[] buttons;
 	private boolean debug;
+	private int clickedIndex;
 
 	public Display(PApplet p) {
 		this.parent = p;
 		debug = true;
+		buttons = new Button[1];
+		
+		buttons[0] = new Button("Test", "this is a test button", 550, 100, 30);
 	}
 
 	public void drawScreen(Engine e) {
 		if (debug) {
 			parent.fill(255);
+			parent.textAlign(parent.LEFT);
 			parent.text("DEBUG", 5, 10);
 			parent.text("timeSpeed: " + e.getTimeSpeed() + " projectiles: "
 					+ e.getProjectiles().size(), 5, 20);
+		}
+		for (Button b : buttons) {
+			b.display(parent);
 		}
 		for (Projectile p : e.getProjectiles()) {
 			if (debug) {
@@ -24,6 +33,10 @@ public class Display {
 			}
 			parent.ellipse(p.getX(), p.getY(), p.getRadius(), p.getRadius());
 		}
+	}
+	
+	public Button[] getButtons() {
+		return buttons;
 	}
 
 	public void switchDebug() {
